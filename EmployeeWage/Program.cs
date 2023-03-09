5e76bf2c05cca6dae3dcb0904bea9e6f60e5dc94
mahesh <lokilokesh1680@gmail.com>
2023-03-09 16:12:33 +0530

    [lokesh]add: EmpWage folder is added

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EmployeeWage
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWage");
+        }
+    }
+}
